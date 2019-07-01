f98eecbb966c22fb86f906e1789ec6e966a04619
drake24 <drakandriel24@gmail.com>
2019-07-02 00:37:35 +0800

    [BACKEND] Removed 'public' in routes;

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,22 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Appetiser</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.2/vue.js"></script>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.2/vue.js"></script>
     </head>
+
     <body>
         <div class="flex-center position-ref full-height" id="app">
-           
-
-            <div class="container">
-                
-                    <calendar></calendar>
-               
+             <div class="container">
+                <calendar></calendar>
             </div>
         </div>
-        <script src="js/app.js"></script>
+        <script src="public/js/app.js"></script>
     </body>
 </html>
